ba3711075432b86df61bd0956a2715390d433a41
Angel <22824872+Zyplos@users.noreply.github.com>
2025-04-24 08:59:37 -0500

    remove console window in release builds

--- main.cpp
+++ main.cpp
@@ -343,6 +343,14 @@ void UserData_WriteAll(ImGuiContext* ctx, ImGuiSettingsHandler* handler, ImGuiTe
 }
 
 
+
+
+//int WINAPI WinMain(
+//    _In_ HINSTANCE hInstance,
+//    _In_opt_ HINSTANCE hPrevInstance,
+//    _In_ LPSTR lpCmdLine,
+//    _In_ int nShowCmd
+//) {
 int main() {
     if (!glfwInit()) {
         std::cout << "GLFW failed\n";
